6d03391f04b28504dc2b5461649f6afa79d675e4
agmota11 <agmota@alumnos.upm.es>
2023-05-14 11:58:09 +0200

    Added settings activity

--- app/src/main/java/es/upm/agmota/proyectomovies/SettingsActivity.java
+++ app/src/main/java/es/upm/agmota/proyectomovies/SettingsActivity.java
@@ -1,14 +1,84 @@
 package es.upm.agmota.proyectomovies;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private EditText editUsername, editPassword;
+    private Button btnSave;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        editUsername = findViewById(R.id.editUsername);
+        editPassword = findViewById(R.id.editPassword);
+        btnSave = findViewById(R.id.btnSave);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveSettings();
+            }
+        });
+    }
+
+    private void saveSettings() {
+        String newUsername = editUsername.getText().toString().trim();
+        String newPassword = editPassword.getText().toString().trim();
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            // Update username
+            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(newUsername)
+                    .build();
+
+            user.updateProfile(profileUpdates)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(SettingsActivity.this, "Username updated successfully.", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(SettingsActivity.this, "Failed to update username.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+            // Update password
+            if (!TextUtils.isEmpty(newPassword)) {
+                user.updatePassword(newPassword)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(SettingsActivity.this, "Password updated successfully.", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(SettingsActivity.this, "Failed to update password.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        }
     }
 }
